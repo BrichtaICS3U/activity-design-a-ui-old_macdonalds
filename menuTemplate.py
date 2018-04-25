907fc89245a722b1848379e8ed37171ead865872
Jacky <jlian5@ocdsb.ca>
2018-04-25 09:07:53 -0400

    Added Loadout

--- menuTemplate.py
+++ menuTemplate.py
@@ -12,7 +12,11 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 200, 0)
 BRIGHT_GREEN = (0, 255, 0)
-RED = (200, 0, 0)
+RED = (200, 50, 0)
+BRIGHT_RED = (255, 50, 0)
+BLUE = (0, 115, 200)
+BRIGHT_BLUE = (0, 115, 255)
+ORANGE = (255, 150, 0)
 
 # Open a new window
 # The window is defined as (width, height), measured in pixels
@@ -26,10 +30,29 @@ pygame.display.set_caption("My Button")
 # --- Text elements
 
 # Define text for title of game
-fontTitle = pygame.font.Font('freesansbold.ttf', 32)
+fontTitle = pygame.font.Font('freesansbold.ttf', 60)
 textSurfaceTitle = fontTitle.render('My Awesome Game!', True, BLACK) 
 textRectTitle = textSurfaceTitle.get_rect()
-textRectTitle.center = (200, 150)   # place the centre of the text
+textRectTitle.center = (SCREENWIDTH/2, SCREENHEIGHT/3) # place the centre of the text
+
+# Button's font
+font = pygame.font.Font ('Comic Sans MS.ttf', 24)
+
+# Play button 
+textFacePlay = font.render ("Play", True, BLACK)
+textRectPlay = textFacePlay.get_rect()
+textRectPlay.center = (SCREENWIDTH/3, SCREENHEIGHT/2 + 25)
+
+# Options button
+textFaceOptions = font.render ("Options", True, BLACK)
+textRectOptions = textFaceOptions.get_rect()
+textRectOptions.center = (SCREENWIDTH*2/3, SCREENHEIGHT/2 + 25)
+
+# Quit button
+textFaceQuit = font.render ("Quit", True, BLACK)
+textRectQuit = textFaceQuit.get_rect()
+textRectQuit.center = (SCREENWIDTH/2, SCREENHEIGHT * 2/3 + 25)
+
 
 # This loop will continue until the user exits the game
 carryOn = True
@@ -60,21 +83,39 @@ while carryOn:
     # Queue shapes to be drawn
     
     # Buttons
-
-    # Green button
+    # Play button
     if SCREENWIDTH/3-50 < mouse[0] < SCREENWIDTH/3+50 and SCREENHEIGHT/2 < mouse[1] < SCREENHEIGHT/2 + 50 and click[0] == 1:
-        pygame.draw.rect(screen, RED, (SCREENWIDTH/3-50, SCREENHEIGHT/2, 100, 50))
-        print('You pressed the button! You maniac!')
+        pygame.draw.rect(screen, ORANGE, (SCREENWIDTH/3-50, SCREENHEIGHT/2, 100, 50))
+        print('Les go')
     elif SCREENWIDTH/3-50 < mouse[0] < SCREENWIDTH/3+50 and SCREENHEIGHT/2 < mouse[1] < SCREENHEIGHT/2 + 50:
         pygame.draw.rect(screen, BRIGHT_GREEN, (SCREENWIDTH/3-50, SCREENHEIGHT/2, 100, 50))
     else:
         pygame.draw.rect(screen, GREEN, (SCREENWIDTH/3-50, SCREENHEIGHT/2, 100, 50))
 
-    # Red button
-    pygame.draw.rect(screen, RED, (SCREENWIDTH*2/3-50, SCREENHEIGHT/2, 100, 50))
+     # Options button
+    if SCREENWIDTH * 2/3 - 50 < mouse[0] < SCREENWIDTH * 2/3 + 50 and SCREENHEIGHT/2 < mouse[1] < SCREENHEIGHT/2 + 50 and click [0] == 1 :
+        pygame.draw.rect(screen, ORANGE, (SCREENWIDTH*2/3-50, SCREENHEIGHT/2, 100, 50))
+        print ("Options")
+    elif SCREENWIDTH * 2/3 - 50 < mouse[0] < SCREENWIDTH * 2/3 + 50 and  SCREENHEIGHT/2 < mouse[1] < SCREENHEIGHT/2 + 50 :
+        pygame.draw.rect (screen, BRIGHT_BLUE, (SCREENWIDTH * 2/3 - 50, SCREENHEIGHT/2, 100, 50))
+    else :
+       pygame.draw.rect (screen, BLUE, (SCREENWIDTH * 2/3 - 50, SCREENHEIGHT/2, 100, 50))
+
+    # Quit button
+    if SCREENWIDTH/2 - 50 < mouse[0] < SCREENWIDTH/2 + 50 and SCREENHEIGHT * 2/3 < mouse[1] < SCREENHEIGHT * 2/3 +50 and click[0] == 1 :
+        pygame.draw.rect (screen, ORANGE, (SCREENWIDTH/2 - 50, SCREENHEIGHT * 2/3, 100, 50))
+        print ("Quit")
+    elif SCREENWIDTH/2 - 50 < mouse[0] < SCREENWIDTH/2 + 50  and SCREENHEIGHT * 2/3 < mouse[1] < SCREENHEIGHT * 2/3 +50 :
+       pygame.draw.rect (screen, BRIGHT_RED, (SCREENWIDTH/2 - 50, SCREENHEIGHT * 2/3, 100, 50))
+    else :
+       pygame.draw.rect (screen, RED, (SCREENWIDTH/2 - 50, SCREENHEIGHT * 2/3, 100, 50))
+
 
     # Text
     screen.blit(textSurfaceTitle, textRectTitle)
+    screen.blit (textFacePlay, textRectPlay)
+    screen.blit (textFaceOptions, textRectOptions)
+    screen.blit (textFaceQuit, textRectQuit)
 
     # Update the screen with queued shapes
     pygame.display.flip()
@@ -84,3 +125,4 @@ while carryOn:
 
 # Once the main program loop is exited, stop the game engine
 pygame.quit()
+
